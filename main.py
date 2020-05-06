603bb0c6797949010b1bdbc578fc96861b6b7886
Ruben <megarex333@gmail.com>
2020-05-07 01:10:45 +0200

    Data plot abstraction

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import data_fetcher.malaga
-import data_fetcher.spain 
+import data_fetcher.spain
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -21,29 +21,31 @@ consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(formatter)
 logger.addHandler(consoleHandler)
 
+
 def date_parse_malaga(x): return pd.datetime.strptime(x, '%d/%m/%Y')
+
+
 def date_parse_spain(x): return pd.datetime.strptime(x, '%Y-%m-%d')
 
 # Actualiza si el CSV tiene más de 30 mins de antiguedad
-try:
-    if time() - getmtime('data/malaga_data.csv') > (30 * 60):
-        logger.info('Updating malaga_data.csv...')
-        data_fetcher.malaga.update_data()
-    else:
-        logger.info("File malaga_data.csv is recent, won't be updated.")
-except:
-    logger.info('File malaga_data.csv not found, downloading...')
-    data_fetcher.malaga.update_data()
 
-try:
-    if time() - getmtime('data/spain_data.csv') > (30 * 60):
-        logger.info('Updating spain_data.csv...')
-        data_fetcher.spain.update_data()
-    else:
-        logger.info("File spain_data.csv is recent, won't be updated.")
-except:
-    logger.info('File spain_data.csv not found, downloading...')
-    data_fetcher.spain.update_data()
+
+def update_csv_if_needed(csv_path, update_function):
+    file_name = csv_path[5:]
+    try:
+        if time() - getmtime(csv_path) > (30 * 60):
+            logger.info('Updating {}...'.format(file_name))
+            update_function()
+        else:
+            logger.info(
+                'File {} is recent, won\'t be updated.'.format(file_name))
+    except:
+        logger.info('File {} not found, downloading...'.format(file_name))
+        update_function()
+
+
+update_csv_if_needed('data/malaga_data.csv', data_fetcher.malaga.update_data)
+update_csv_if_needed('data/spain_data.csv', data_fetcher.spain.update_data)
 
 # Get the data
 column_names = ['casos_total', 'altas',
@@ -52,9 +54,9 @@ url = 'https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/na
 
 logger.info('Reading data...')
 df_spain = pd.read_csv('data/spain_data.csv', delimiter=',',
-    parse_dates=['fecha'], date_parser=date_parse_spain)
+                       parse_dates=['fecha'], date_parser=date_parse_spain)
 df_malaga = pd.read_csv('data/malaga_data.csv', delimiter=',',
-    parse_dates=['fecha'], date_parser=date_parse_malaga)
+                        parse_dates=['fecha'], date_parser=date_parse_malaga)
 logger.info('Data read sucessfully.')
 
 for column_name in column_names:
@@ -70,6 +72,7 @@ def moving_average(data, n=7):
     for i in range(7, size):
         averages.append(sum(data[i-7:i])/n)
     return averages
+
 
 logger.info('Computing metrics for plot.')
 # Compute data
@@ -96,6 +99,25 @@ diff_cases_malaga = np.insert(np.diff(cases_malaga), 0, 0)
 diff_deaths_malaga = np.insert(np.diff(deaths_malaga), 0, 0)
 
 
+# Data join
+data = {
+    'Spain': {
+        'date': df_spain['fecha'],
+        'actives': active_spain,
+        'actives difference': diff_active_spain,
+        'deaths': deaths_spain,
+        'cured': cured_spain,
+    },
+    'Málaga': {
+        'date': df_malaga['fecha'],
+        'actives': active_malaga,
+        'actives difference': diff_active_malaga,
+        'deaths': deaths_malaga,
+        'cured': cured_malaga
+    }
+}
+
+
 # Plot the data
 logger.info('Data ready for plotting.')
 logger.info('Starting plotting.')
@@ -116,130 +138,73 @@ plt.rcParams['axes.linewidth'] = 0.8
 plt.rcParams['xtick.color'] = '#212121'
 plt.rcParams['ytick.color'] = '#212121'
 
-
-# ----------------------------------------------------------------
-ax = plt.subplot(221)
-
-ax.set_axisbelow(True)
-plt.grid(color='#DDDDDD', linestyle='-', linewidth=0.8, axis='y')
-
-ax.set_title('Accumulated actives, deaths and recovered', fontsize=20)
-
-plt.tick_params(axis='x', which='major', labelsize=15)
-plt.tick_params(axis='y', which='both', left=False, right=False, labelsize=15)
-
-ax.spines['top'].set_color('none')
-ax.spines['right'].set_color('none')
-ax.spines['left'].set_color('none')
-ax.spines['bottom'].set_smart_bounds(True)
-
+n = 0
 data_colors = ['#001b3f', '#05638f', '#6eaedf', '#d00005']
 
-ax.bar(df_spain['fecha'], active_spain, label='Actives', color=data_colors[0])
-ax.bar(df_spain['fecha'], deaths_spain, label='Deaths',
-       color=data_colors[1], bottom=active_spain)
-ax.bar(df_spain['fecha'], cured_spain, label='Recovered',
-       color=data_colors[2], bottom=active_spain+deaths_spain)
 
-ax.legend()
-
-ax.xaxis.set_major_locator(mdates.WeekdayLocator())
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b'))
-
-
-# ----------------------------------------------------------------
-ax = plt.subplot(222)
-
-ax.set_axisbelow(True)
-plt.grid(color='#DDDDDD', linestyle='-', linewidth=0.8, axis='y')
-
-ax.set_title('Accumulated actives, deaths and recovered (Málaga)', fontsize=20)
+def plot_init():
+    global n
+    n += 1
+    ax = plt.subplot(220 + n)
+    ax.set_axisbelow(True)
+    plt.grid(color='#DDDDDD', linestyle='-', linewidth=0.8, axis='y')
+    plt.tick_params(axis='x', which='major', labelsize=15)
+    plt.tick_params(axis='y', which='both', left=False,
+                    right=False, labelsize=15)
+    ax.spines['top'].set_color('none')
+    ax.spines['right'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.spines['bottom'].set_smart_bounds(True)
+    ax.xaxis.set_major_locator(mdates.WeekdayLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b'))
+    return ax
 
 
-plt.tick_params(axis='x', which='major', labelsize=15)
-plt.tick_params(axis='y', which='both', left=False, right=False, labelsize=15)
-
-ax.spines['top'].set_color('none')
-ax.spines['right'].set_color('none')
-ax.spines['left'].set_color('none')
-ax.spines['bottom'].set_smart_bounds(True)
-
-
-ax.bar(df_malaga['fecha'], active_malaga,
-       label='Actives', color=data_colors[0])
-ax.bar(df_malaga['fecha'], deaths_malaga,
-       label='Deaths', color=data_colors[1], bottom=active_malaga)
-ax.bar(df_malaga['fecha'], cured_malaga,
-       label='Recovered', color=data_colors[2], bottom=active_malaga + deaths_malaga)
-
-ax.legend()
-
-ax.xaxis.set_major_locator(mdates.WeekdayLocator())
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b'))
+def accumulated_plot(location):
+    ax = plot_init()
+    ax.set_title(
+        'Accumulated actives, deaths and recovered ({})'.format(location), fontsize=20)
+    # Data extraction
+    date = data[location]['date']
+    actives = data[location]['actives']
+    deaths = data[location]['deaths']
+    cured = data[location]['cured']
+    # ------------------------------------
+    ax.bar(date, actives,
+           label='Actives', color=data_colors[0])
+    ax.bar(date, deaths,
+           label='Deaths', color=data_colors[1], bottom=actives)
+    ax.bar(date, cured,
+           label='Cured', color=data_colors[2], bottom=actives+deaths)
+    ax.legend(loc='upper left')
 
 
-# ----------------------------------------------------------------
-ax = plt.subplot(223)
-
-ax.set_axisbelow(True)
-plt.grid(color='#EEEEEE', linestyle='-', linewidth=0.8, axis='y')
-
-ax.set_title('Active cases variation', fontsize=20)
-
-plt.tick_params(axis='x', which='major', labelsize=15)
-plt.tick_params(axis='y', which='both', left=False, right=False, labelsize=15)
-
-ax.spines['top'].set_color('none')
-ax.spines['right'].set_color('none')
-ax.spines['left'].set_color('none')
-ax.spines['bottom'].set_smart_bounds(True)
-
-ax.plot(df_spain['fecha'], [0] * len(df_spain['fecha']),
-        color='212121', lw='2', linestyle=':')
-ax.bar(df_spain['fecha'], diff_active_spain, lw=2.5,
-       color=data_colors[0], label='Actives variation')
-
-
-ax.plot(df_spain['fecha'], moving_average(diff_active_spain), lw=2.5,
-        color=data_colors[3], label='Moving average of actives variation')
-
-ax.legend(loc='upper left')
-
-ax.xaxis.set_major_locator(mdates.WeekdayLocator())
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b'))
+def variation_plot(location):
+    ax = plot_init()
+    ax.set_title('Active cases variation ({})'.format(location), fontsize=20)
+    # Data extraction
+    date = data[location]['date']
+    actives_difference = data[location]['actives difference']
+    # ------------------------------------
+    ax.plot(data[location]['date'], [0] * len(data[location]['date']),
+            color='212121', lw='2', linestyle=':')
+    ax.bar(data[location]['date'], actives_difference, lw=2.5,
+           color=data_colors[0], label='Actives variation')
+    ax.plot(data[location]['date'], moving_average(actives_difference), lw=2.5,
+            color=data_colors[3], label='Moving average of actives variation')
+    ax.legend(loc='upper left')
 
 
 # ----------------------------------------------------------------
-ax = plt.subplot(224)
-
-ax.set_axisbelow(True)
-plt.grid(color='#EEEEEE', linestyle='-', linewidth=0.8, axis='y')
-
-ax.set_title('Active cases variation (Málaga)', fontsize=20)
-
-plt.tick_params(axis='x', which='major', labelsize=15)
-plt.tick_params(axis='y', which='both', left=False, right=False, labelsize=15)
-
-ax.spines['top'].set_color('none')
-ax.spines['right'].set_color('none')
-ax.spines['left'].set_color('none')
-ax.spines['bottom'].set_smart_bounds(True)
-
-ax.plot(df_malaga['fecha'], [0] * len(df_malaga['fecha']),
-        color='212121', lw='2', linestyle=':')
-ax.bar(df_malaga['fecha'], diff_active_malaga, lw=2.5,
-       color=data_colors[0], label='Actives variation')
-
-ax.plot(df_malaga['fecha'], moving_average(diff_active_malaga), lw=2.5,
-        color=data_colors[3], label='Moving average of actives variation')
-
-ax.legend(loc='upper left')
-
-ax.xaxis.set_major_locator(mdates.WeekdayLocator())
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b'))
-
-
+accumulated_plot('Spain')
 # ----------------------------------------------------------------
+accumulated_plot('Málaga')
+# ----------------------------------------------------------------
+variation_plot('Spain')
+# ----------------------------------------------------------------
+variation_plot('Málaga')
+# ----------------------------------------------------------------
+
 fig.text(0.13, 0.06, "Data source: https://github.com/datadista/datasets"
                      "\nAuthor: Oleg Brezitskyy (@oleg.brz)", fontsize=10)
 
